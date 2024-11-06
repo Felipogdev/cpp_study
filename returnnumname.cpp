0fbb00d3d906e40d10fb609ae29e81fb63679f15
Felipe Dantas Laurentino <felipedantaslaurentino@gmail.com>
2024-11-06 01:44:58 +0000

    x

---
+++ returnnumname.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    switch (n) {
+        case 1:
+            cout << "one";
+            break;
+        case 2:
+            cout << "two";
+            break;
+        case 3:
+            cout << "three";
+            break;
+        case 4:
+            cout << "four";
+            break;
+        case 5:
+            cout << "five";
+            break;
+        case 6:
+            cout << "six";
+            break;
+        case 7:
+            cout << "seven";
+            break;
+        case 8:
+            cout << "eight";
+            break;
+        case 9:
+            cout << "nine";
+            break;
+    }
