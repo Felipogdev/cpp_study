4d98e2e111e073d90d243383c6000248eaeeca68
Felipe Dantas Laurentino <felipedantaslaurentino@gmail.com>
2024-11-10 18:34:57 +0000

    FinalCommit

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,11 +2,9 @@
 #include <time.h>
 using namespace std;
 
-//funcao checkwin
-//funcao checktie
-
 void printBoard (char arr[][3]);
 bool checkWin (char arr[3][3]);
+bool checkTie (char arr[3][3]);
 
 int main() {
     char board[3][3];
@@ -27,15 +25,26 @@ int main() {
          cout << "Jogador",(playerTurn) ? cout << " X " : cout << " O ";
          cout << "Digite a celula que gostaria de marcar: ";
          cin >> cell;
-         } while (cell < 1 || cell > 9);
-         // TODO: Checar se a posição já foi utilizada.
-         int cellX = (cell % 3);
-         int cellY = (cell / 3);
+         } while (cell < 1 || cell > 9 || board[(cell-1)/3][(cell-1)%3] != '.');
+         int cellX = (cell -1) / 3;
+         int cellY = (cell -1) % 3;
          if (playerTurn == true) board[cellX][cellY] = 'X';
          if (playerTurn == false) board[cellX][cellY] = 'O';
 
-         turn++;
-         if (turn > 3) checkWin(board);
+         if (checkWin(board)) {
+             printBoard(board);
+             cout<< "Jogador ";
+             (playerTurn) ? cout << " X " : cout<< " O ";
+             cout<< "venceu!";
+             break;
+         }
+
+         if (checkTie(board)) {
+             printBoard(board);
+             cout << "Deu Velha!";
+             break;
+         }
+
          //Para mudar o turno
          playerTurn = !playerTurn;
 
@@ -56,16 +65,25 @@ void printBoard (char arr[3][3]) {
 
 bool checkWin (char arr[3][3]) {
     //Wins na Horizontal
-    if (arr [0][0] == arr[0][1] && arr[0][1] == arr[0][2]) return true;
-    if (arr [1][0] == arr[1][1] && arr[1][1] == arr[1][2]) return true;
-    if (arr [2][0] == arr[2][1] && arr[2][1] == arr[2][2]) return true;
+    for (int i = 0; i < 3; i++) {
+    if (arr [i][0] != '.' && arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2]) return true;
+
+    }
     //Wins na Vertical
-    if (arr [0][0] == arr[1][0] && arr[1][0] == arr[2][0]) return true;
-    if (arr [0][1] == arr[1][1] && arr[1][1] == arr[2][1]) return true;
-    if (arr [0][2] == arr[1][2] && arr[1][2] == arr[2][2]) return true;
+    for (int i = 0; i < 3; i++) {
+        if (arr[0][i] != '.' && arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i]) return true;
+    }
     //Wins na Diagonal
-    if (arr [0][0] == arr[1][1] && arr[1][1] == arr[2][2]) return true;
-    if (arr [0][2] == arr[1][1] && arr[1][1] == arr[2][0]) return true;
+    if (arr[0][0] != '.' && arr [0][0] == arr[1][1] && arr[1][1] == arr[2][2]) return true;
+    if (arr[0][2] != '.' && arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0]) return true;
     return false;
 }
 
+bool checkTie (char arr[3][3]) {
+    for(int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (arr[i][j] == '.') return false;
+        }
+    }
+    return true;
+}
